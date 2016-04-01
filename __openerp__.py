7dd7dfe8572227a5a7ae9df7ecf10cca6583909c
irisienne <roumani.rahma@mail.com>
2016-04-01 15:10:57 +0100

    test

--- __openerp__.py
+++ __openerp__.py
@@ -18,8 +18,8 @@
 	],
 
 	'installable': True,
-        'application': True,
-	'auto_install': False,
+    'application': True,
+    #this was my changes
 }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
